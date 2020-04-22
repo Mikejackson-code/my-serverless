2e63d9d9a97538acdb340c4de4f93e64fc99015f
Mike Jackson <jackson-mike@sbcglobal.net>
2020-04-22 14:37:53 -0400

    correction for file definitions

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'pksye2u9k1'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/Dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-8hd7xefk.auth0.com',            // Auth0 domain
+  clientId: 'a0vw5Q9Zjx0rbVUbiqjHZ4KrBNZeqKBD',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
